c5168221c01e26955bfaababfc739432e70087af
Vlad <bvladimix@gmail.com>
2023-04-26 14:27:43 -0400

    github access token

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,13 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const GITHUB_API_URL = 'https://api.github.com/graphql';
-const GITHUB_PERSONAL_ACCESS_TOKEN = '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'; // Replace with your own token
 
 const client = new ApolloClient({
   uri: GITHUB_API_URL,
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer ${GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
   },
 });
 
